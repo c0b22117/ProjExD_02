3c2c3798de96b0906eaaf9ac2f79ac4253989296
harunari <c0b22117b2@edu.teu.ac.jp>
2023-10-03 14:56:15 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,27 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
+
+
+def check_bound(obj_rct):
+    """
+    引数：こうかとんRectばくだんRect
+    戻り値：タプル（横方向判定結果、縦方向判定結果）
+    画面内ならTrue：画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +33,9 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400)
+    
     bd_img = pg.Surface((20, 20))
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
@@ -26,9 +50,22 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        
+        """工科トン"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+                
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+        
+        
+        """ばくだん"""
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct)
         pg.display.update()
