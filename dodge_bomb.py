5bad5d07c8469319d60d62418a4acf8850d95482
harunari <c0b22117b2@edu.teu.ac.jp>
2023-10-03 17:22:28 +0900

    修正1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,7 +5,7 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
-delta = {
+delta = { #移動量タプル
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
@@ -13,16 +13,17 @@ delta = {
 }
 
 
-def check_bound(obj_rct):
+
+def check_bound(obj_rct):#pg.Rectのインスタンスが来ることを指定
     """
     引数：こうかとんRectばくだんRect
     戻り値：タプル（横方向判定結果、縦方向判定結果）
     画面内ならTrue：画面外ならFalse
     """
     yoko, tate = True, True
-    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:#横方向判定
         yoko = False
-    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:#縦方向判定
         tate = False
     return yoko, tate
 
@@ -31,17 +32,33 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)#２倍に拡大
+    kk_rct = kk_img.get_rect()#SurfaceからRectを抽出する
     kk_rct.center = (900,400)
     
-    bd_img = pg.Surface((20, 20))
-    bd_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    bd_rct = bd_img.get_rect()
-    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    bd_rct.center = (x, y)
+    kk_img2 = pg.transform.flip(kk_img,True,False)
+    
+    kk_d = {
+        (0,-5):pg.transform.rotozoom(kk_img2,90,1.0),
+        (+5,-5):pg.transform.rotozoom(kk_img2,45,1.0),
+        (+5,0):kk_img2,
+        (+5,+5):pg.transform.rotozoom(kk_img2,315,1.0),
+        (0,+5):pg.transform.rotozoom(kk_img2,270,1.0),
+        (-5,+5):pg.transform.rotozoom(kk_img,90,1.0),
+        (-5,0):pg.transform.rotozoom(kk_img,0,1.0),
+        (-5,-5):pg.transform.rotozoom(kk_img,315,1.0),
+        (0,0):kk_img,
+    }
+    
+    """ばくだん"""
+    bd_img = pg.Surface((20, 20))#爆弾Surfaceをつくる
+    bd_img.set_colorkey((0, 0, 0))#黒を投下させる
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)#中心に半径１０の赤い円
+    bd_rct = bd_img.get_rect()#SurfaceからRectを抽出する
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)#座標をランダムに生成
+    bd_rct.center = (x, y)#Rectにランダムな座標を設定する
     vx,vy = +5, +5
     
     clock = pg.time.Clock()
@@ -57,25 +74,32 @@ def main():
         screen.blit(bg_img, [0, 0])
         
         """工科トン"""
-        key_lst = pg.key.get_pressed()
+        key_lst = pg.key.get_pressed()#全てのキーの押下状態を取得する
         sum_mv = [0, 0]
         for key, mv in delta.items():
-            if key_lst[key]:
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
+            if key_lst[key]:#キーが押されたら
+                sum_mv[0] += mv[0] #横方向の移動量
+                sum_mv[1] += mv[1] #縦方向の移動量
+        kk_img = kk_d[tuple(sum_mv)]
                 
+         
+        
         kk_rct.move_ip(sum_mv[0],sum_mv[1])
-        if check_bound(kk_rct) != (True,True):
+        if check_bound(kk_rct) != (True,True):#画面外に出ないように制限
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+            
+ 
         screen.blit(kk_img, kk_rct)
         
         
+        
+        
         """ばくだん"""
-        bd_rct.move_ip(vx,vy)
+        bd_rct.move_ip(vx,vy) #爆弾を移動させる
         yoko, tate = check_bound(bd_rct)
-        if not yoko:
+        if not yoko: #横方向にはみ出したら
             vx *= -1
-        if not tate:
+        if not tate: #縦方向にはみ出したら
             vy *= -1
         screen.blit(bd_img,bd_rct)
         
